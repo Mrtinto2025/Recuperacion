e809eaa6c4fb1300caa4915ba0d631ea9b923f66
jhorman segura <jhormansegura2012@gmail.com>
2025-04-20 15:10:08 -0500

    Cambiomasivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
-import Principal from './Component/Principal/Principal';
-import Login from './Component/Login/Login';
-import Blog from './Component/Blog/Blog';
-import Registro from './Component/Registro/Registro';
-import Usuario from './Component/Usuario/Usuario';
+import Principal from './Components/Principal/Principal';
+import Login from './Components/Login/Login';
+import Usuario from './Components/Usuario/Usuario';
+import Registro from './Components/Registro/Registro';
+import Blog from './Components/Blog/Blog';
 
 
 function App() {
   const [Count, SetCount] = useState(0)
     return(
       <Routes>
+        <Route path='/' element={<Principal></Principal>}></Route>
         <Route path='/Login' element={<Login></Login>}></Route>
         <Route path='/Usuario' element={<Usuario></Usuario>}></Route>
-        <Route path='/' element={<Principal></Principal>}></Route>
         <Route path='/Registro' element={<Registro></Registro>}></Route>
         <Route path='/Blog' element={<Blog></Blog>}></Route>
       </Routes>
--- src/Components/Principal/Principal.jsx
+++ src/Components/Principal/Principal.jsx
@@ -1,22 +1,21 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Card from 'react-bootstrap/Card';
-import React from 'react';
+
 
 const Principal = () => {
   return (
-  <>
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Remontada</Navbar.Brand>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Blog</Nav.Link>
-            <Nav.Link href="#link">Menu</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -33,29 +32,11 @@ const Principal = () => {
       </Container>
     </Navbar>
     </div>
-
-    <div className='titulo'>
-      <h2>Trateme serio</h2>
-    </div>
-
-    <Card style={{ width: '18rem' }}>      
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-        En la penumbra de la noche, las estrellas susurran secretos al viento, mientras la luna, 
-        cual amante distante, ilumina con su suave resplandor los caminos del alma.
-        </Card.Text>
-        <Button variant="primary">Next</Button>
-      </Card.Body>
-    </Card>
-
-    
-    </>
-
   );
 }
 
 export default Principal;
+
 
 
  
